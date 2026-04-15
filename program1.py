a77e96dffeae7aa12d56fa57331dbe1c1dd410aa
Tanishkkalaskar07 <tanishk,kalaskar.aids.2023@vpkbiet.org>
2026-04-15 13:30:08 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,6 +1,7 @@
 num=int(input("Enter the number:"))
-
+print("User has entered the number")
 if num%2==0:
     print(num ,"is even")
 else:
     print(num ,"is odd")
+print("Program succesfully executed")
